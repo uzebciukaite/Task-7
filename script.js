cf562bacb12fb1bcae21787c6eacfc446117ba95
Urte Zebciukaite <102223171+uzebciukaite@users.noreply.github.com>
2022-05-27 14:38:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  marke: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+
+
+function showObjectKeys(array){
+ var showKeys = Object.keys(array)
+ console.log(showKeys)
+}
+
+showObjectKeys(audi)
